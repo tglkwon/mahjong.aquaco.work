3cf6786706e14e4a3881430a5620070b15a0fd1f
AquaCo <tglkwon@gmail.com>
2022-05-26 08:06:56 +0000

    score page inputs 220526

--- src/page/Score.js
+++ src/page/Score.js
@@ -8,10 +8,17 @@ function Write() {
   const navigate = useNavigate()
   const [ email, setEmail ] = useState("")
   const [ password, setPassword ] = useState("")
-  const [ title, setTitle ] = useState("")
-  const [ contents, setContents] = useState("")
+  const [ namePlayer1, setNamePlayer1 ] = useState("")
+  const [ namePlayer2, setNamePlayer2 ] = useState("")
+  const [ namePlayer3, setNamePlayer3 ] = useState("")
+  const [ namePlayer4, setNamePlayer4 ] = useState("")
+  const [ scorePlayer1, setScorePlayer1 ] = useState("")
+  const [ scorePlayer2, setScorePlayer2 ] = useState("")
+  const [ scorePlayer3, setScorePlayer3 ] = useState("")
+  const [ scorePlayer4, setScorePlayer4 ] = useState("")
 
 
+  
   async function writeAxios() {
     const { data } = await axios({
       method: 'POST',
@@ -19,8 +26,14 @@ function Write() {
       data: {
         author: email,
         password: password,
-        title: title,
-        contents: contents     
+        namePlayer1: namePlayer1,
+        namePlayer2: namePlayer2,
+        namePlayer3: namePlayer3,
+        namePlayer4: namePlayer4,
+        scorePlayer1: scorePlayer1,
+        scorePlayer2: scorePlayer2,
+        scorePlayer3: scorePlayer3,
+        scorePlayer4: scorePlayer4
       }
     })
     if (data.success) {
@@ -29,11 +42,26 @@ function Write() {
   }
 
   return (
-    <div className="Write">
-      <div className='author'>
-        <input className='email' type='text' value={email} onChange={event => {setEmail(event.target.value)}} placeholder='email'></input>
-        <input className='password' type='password' value={password} onChange={event => {setPassword(event.target.value)}} placeholder='Password'></input>
-        <input className='title' type='text' value={title} onChange={event => {setTitle(event.target.value)}} placeholder='Title'></input>  
+    <div className="page">
+      <div className='names'>
+
+      </div>
+      <div className='inputs'>
+        <div className='uploadImage'>
+
+        </div>
+        <div className='inputscores'>
+          <input className='scorePlayer1' type='text' value={scorePlayer1} onChange={event => {setScorePlayer1(event.target.value)}}></input>
+          <input className='scorePlayer2' type='text' value={scorePlayer2} onChange={event => {setScorePlayer2(event.target.value)}}></input>
+          <input className='scorePlayer3' type='text' value={scorePlayer3} onChange={event => {setScorePlayer3(event.target.value)}}></input>
+          <input className='scorePlayer4' type='text' value={scorePlayer4} onChange={event => {setScorePlayer4(event.target.value)}}></input>    
+        </div>
+        <div className='inputnames'>
+          <input className='namePlayer1' type='text' value={namePlayer1} onChange={event => {setNamePlayer1(event.target.value)}}></input>
+          <input className='namePlayer2' type='text' value={namePlayer2} onChange={event => {setNamePlayer2(event.target.value)}}></input>
+          <input className='namePlayer3' type='text' value={namePlayer3} onChange={event => {setNamePlayer3(event.target.value)}}></input>
+          <input className='namePlayer4' type='text' value={namePlayer4} onChange={event => {setNamePlayer4(event.target.value)}}></input>
+        </div>  
       </div>
     </div>
   )
